862cd7b0136c6a0ddd7967a646c485809ec4bb3e
Jacobwlarsen <jacobwlarsendk@gmail.com>
2017-03-19 22:29:43 +0100

    Added quote section to website...Something else with including jQuery in project and beginning to work with javascript.

---
+++ TheWorld/wwwroot/js/mainscript.js
@@ -0,0 +1,24 @@
+//Main Scripts
+
+
+
+//Self-executing anonymous function to avoid name collision
+(function() {
+    //var ele = $("#note");
+    //ele.text("Site coming soon - March 2017 v1.01");
+
+    //var header = $("#lheader");
+    //header.on("mouseenter", function () {
+    //    header.text("What's up?");
+    //});
+    //header.on("mouseleave", function () {
+    //    header.text("Current Learning Topics");
+    //});
+
+    //var menuItems = $("ul.nav__ul a li");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert("Hello Jake! You clicked the " + me.text() + " link!");
+    //})
+
+})();
